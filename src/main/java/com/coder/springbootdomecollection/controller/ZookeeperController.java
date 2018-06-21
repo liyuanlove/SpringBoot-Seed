d6f05404a2700c546fd8c280027a5c2d0f54efd7
吴俊龙 <735626035@qq.com>
2018-06-21 14:22:04 +0800

    此版本集成Zookeeper

---
+++ src/main/java/com/coder/springbootdomecollection/controller/ZookeeperController.java
@@ -0,0 +1,26 @@
+package com.coder.springbootdomecollection.controller;
+
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ZookeeperController {
+
+    @RequestMapping(value="/testZookeeper")
+    public String testZookeeper(){
+        Watcher watcher = (event) ->{System.out.println("receive event:" + event);};
+        String value = null;
+        try{
+            final ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1:2181",2000,watcher);
+            final byte[] data = zooKeeper.getData("/node_1",watcher,null);
+            value = new String(data);
+            zooKeeper.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "get value:" + value;
+    }
+
+}
