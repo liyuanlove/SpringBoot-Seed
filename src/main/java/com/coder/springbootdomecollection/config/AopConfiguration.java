78cefcdb9f18cbb26aa3257d1d5b1d72628ef2bf
吴俊龙 <735626035@qq.com>
2018-09-12 09:16:01 +0800

    研究双色球

--- src/main/java/com/coder/springbootdomecollection/config/AopConfiguration.java
+++ src/main/java/com/coder/springbootdomecollection/config/AopConfiguration.java
@@ -1,4 +1,4 @@
-package com.coder.springbootdomecollection.aop;
+package com.coder.springbootdomecollection.config;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,39 +10,62 @@ import org.springframework.stereotype.Component;
 import javax.sound.midi.Soundbank;
 import java.sql.SQLOutput;
 
-@Aspect
+
 /**
  * 定义切面
  * */
-@Configuration
+@Aspect
 /**
  * 注册组件
  */
+@Configuration
 public class AopConfiguration {
 
+    /**
+     * 切入点
+     */
     @Pointcut("execution(* com.coder..serviceimpl.*.*(..))")
     public void executeService(){ }
 
+    /**
+     * 在切入点之前
+     * @param jp
+     */
     @Before("executeService()")
     public void before(JoinPoint jp){
         System.out.println("执行方法之前执行");
     }
 
+    /**
+     * 在切入点之后
+     * @param jp
+     */
     @AfterReturning("executeService()")
     public void after(JoinPoint jp){
         System.out.println("执行方法之后执行");
     }
 
+    /**
+     * 在切入点抛出异常之后
+     */
     @AfterThrowing("executeService()")
     public void afterthrowing(){
         System.out.println("方法抛出异常执行");
     }
 
+    /**
+     * 在切入点正常返回之后
+     */
     @AfterReturning("executeService()")
     public void afterReturning(){
         System.out.println("方法正常返回之后执行");
     }
 
+    /**
+     * 环绕通知
+     * @param pjp
+     * @return
+     */
     @Around("execution(* com.coder.*.serviceimpl.*.*(..))")
     public Object around(ProceedingJoinPoint pjp){
         System.out.println("环绕通知");
